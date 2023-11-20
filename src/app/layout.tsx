8ec976919d5586834c0610d17bb49d3b77a89795
johanse7 <jhoan.sanchez@valtech.com>
2023-11-20 12:47:46 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-
+export const dynamic = 'force-dynamic'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
